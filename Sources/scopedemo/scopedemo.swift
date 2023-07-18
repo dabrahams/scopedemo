bcef32fec402871c07bbed8c7d6c479b7de06d14
Dave Abrahams <dabrahams@adobe.com>
2023-07-18 15:42:51 -0700

    Simplify the repro

--- Sources/scopedemo/scopedemo.swift
+++ Sources/scopedemo/scopedemo.swift
@@ -1,3 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-public struct Type {}
+struct Type {}
+struct Foo {}
+extension scopedemo.Foo {} // OK
+extension Type {} // OK
+extension scopedemo.Type {} // error: cannot find type 'scopedemo' in scope
--- Sources/scopedemo/subdirectory/x.swift
+++
@@ -1,3 +0,0 @@
-public struct Foo {
-  public var x: scopedemo.Type
-}
